70fb3f753eec8d0cb260ddea40e02ce23bb985a7
sachin sapkota <sachinsapkota773@gmail.com>
2022-06-21 11:00:31 +0545

    changes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import router from './routes';
 const next = require('next');
-
+const cors = require('cors');
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 app.prepare().then(() => {
+  const userrouter = require('./routes/user');
   const signuproute = require('./routes/signup');
   const server = express();
   server.use(bodyParser.json());
-  server.use(cookieParser());
-  server.use(express.json());
-  server.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-  server.use(router);
 
+  server.use('/user', userrouter);
+  server.post('/sign-up', signuproute);
   server.post('/api', (req, res) => {
     return app.render(req, res, '/api', req.query);
   });
